ce90a7e65ca27d3b35259fef5400d82ba33eb262
unknown <marronturtle0403@yahoo.co.jp>
2019-06-02 19:51:59 +0900

    Add Report show

--- src/controllers/reports/ReportsCreateServlet.java
+++ src/controllers/reports/ReportsCreateServlet.java
@@ -21,7 +21,7 @@ import utils.DBUtil;
 /**
  * Servlet implementation class ReportsCreateServlet
  */
-@WebServlet("/reports.create")
+@WebServlet("/reports/create")
 public class ReportsCreateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
